0ea5702168db6e79c34184a255cc0eb1b2ad1771
sharifulkawsar <mdsharifulkawsar@gmail.com>
2022-09-27 00:27:51 +0600

    final commit

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Helpers\Database;
+use App\Helpers\ResponseHelper;
+use App\Models\Post;
+
+class HomeController
+{
+    public function index()
+    {
+        $database = Database::getInstance();
+        return ResponseHelper::renderView('master' );
+    }
+
+    public function edit()
+    {
+        return ResponseHelper::renderView('edit' );
+    }
+
+    public function editPost()
+    {
+        $id = $_REQUEST['id'];
+        $data = new Post();
+        $result = $data->getPost($id);
+        return $result;
+    }
+
+    public function updatePost()
+    {
+        $post = new Post();
+        $post->updatePost($_POST);
+        header('Location: /dashboard');
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use App\Helpers\Database;
+
+class Post
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $this->connection = Database::getInstance();
+    }
+
+    public function createPost(array $post)
+    {
+        // var_dump("Hello");
+        // exit;
+        $title = $post['title'];
+        $author = $post['author'];
+        $image = $post['image'];
+
+        // do store the user data into database
+        $queryURI = "INSERT INTO posts (title, author, image) VALUES (:title, :author, :image)";
+        $query = $this->connection->prepare($queryURI);
+        $query->bindValue(":title", $title);
+        $query->bindValue(":author", $author);
+        $query->bindValue(":image", $image);
+
+        return $query->execute();
+
+        // var_dump($title, $author, $image);
+        // exit;
+    }
+
+    public function getAllPosts()
+    {
+        $queryURI = "SELECT * FROM posts";
+        $query = $this->connection->prepare($queryURI);
+        $query->execute();
+        return $query->fetchAll($this->connection::FETCH_ASSOC);
+    }
+
+    public function getPost($id)
+    {
+        $queryURI = "SELECT * FROM posts WHERE id = $id";
+        $query = $this->connection->prepare($queryURI);
+        $query->execute();
+        return $query->fetchAll($this->connection::FETCH_ASSOC);
+    }
+
+    public function updatePost(array $post)
+    {
+        $id = $post['id'];
+        $title = $post['title'];
+        $author = $post['author'];
+        $image = $post['image'];
+
+        // do store the user data into database
+        $queryURI = "UPDATE posts SET title = :title, author = :author, image = :image WHERE id = $id";
+
+        $query = $this->connection->prepare($queryURI);
+        $query->bindValue(":title", $title);
+        $query->bindValue(":author", $author);
+        $query->bindValue(":image", $image);
+
+        return $query->execute();
+    }
+};
